4efd907ed70de6b31d3e1dac5fa7519c90c77410
unknown <maheshtech21@gmail.com>
2025-10-07 20:44:46 +0530

    updated app file

--- Node/app.js
+++ Node/app.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World! from AKS\n');
+  res.end('Hello, World! from AKS!!\n');
 });
 
 server.listen(port, '0.0.0.0', () => {
